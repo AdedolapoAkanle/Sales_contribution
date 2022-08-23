8cf7879783b3b8b0eedaa7afcf85a85a8cacc866
AdedolapoAkanle <dolapoakanle@gmail.com>
2022-08-23 10:51:35 +0100

    new commit

--- Model/View/dashboard.php
+++ Model/View/dashboard.php
@@ -21,7 +21,7 @@ if (isset($_GET['error'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sales Contribution</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 
     <link rel="stylesheet" href="style.css">
 </head>
@@ -112,6 +112,7 @@ if (isset($_GET['msg'])) {
     //     })
     // }
     </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </body>
 
 </html>
